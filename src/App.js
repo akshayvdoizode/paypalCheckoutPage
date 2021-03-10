ec6c9213ef2edd8a6ec11a5d0c01cb62dcf057d6
akshay v doyizode <akshayvdoizode123@gmail.com>
2021-03-10 12:19:26 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+function App() {
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "0.01",
+          },
+        },
+      ],
+    });
+  };
+  const onApprove = (data, actions) => {
+    return actions.order.capture();
+  };
+  return (
+    <div className="app">
+      <div className="wrapper">
+        <PayPalButton
+          createOrder={(data, actions) => createOrder(data, actions)}
+          onApprove={(data, actions) => onApprove(data, actions)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
